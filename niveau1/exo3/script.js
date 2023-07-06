51a620074d9b0462f57ee91ed5b6aec0c573654f
light419 <refesseteddy@gmail.com>
2023-07-06 14:36:48 +0200

    exo num3 commit

---
+++ niveau1/exo3/script.js
@@ -0,0 +1,4 @@
+const nombre1 = parseInt(prompt("Entrez le premier nombre :"));
+const nombre2 = parseInt(prompt("Entrez le deuxième nombre :"));
+const somme = nombre1 + nombre2;
+console.log("La somme des deux nombres est : " + somme);
